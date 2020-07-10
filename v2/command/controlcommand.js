098baa8ba0c47f4b4bfefa685d6963bb847cc248
joaomgcd <support@joaoapps.com>
2020-07-10 22:10:47 +0100

    Added keyboard icons for future shortcuts

--- v2/command/controlcommand.js
+++ v2/command/controlcommand.js
@@ -61,6 +61,7 @@ export class ControlCommand extends Control {
             <div class="commandiconwrapper"></div>
             <a class='buttonlink' ></a>
             <a class='buttonlinkextended' ></a>
+            <svg class="commandkeyboardshortcut" style="width:24px;height:24px" viewBox="0 0 24 24"><path d="M4,5A2,2 0 0,0 2,7V17A2,2 0 0,0 4,19H20A2,2 0 0,0 22,17V7A2,2 0 0,0 20,5H4M4,7H20V17H4V7M5,8V10H7V8H5M8,8V10H10V8H8M11,8V10H13V8H11M14,8V10H16V8H14M17,8V10H19V8H17M5,11V13H7V11H5M8,11V13H10V11H8M11,11V13H13V11H11M14,11V13H16V11H14M17,11V13H19V11H17M8,14V16H16V14H8Z" /></svg>
         </div>`;
     }
     async renderSpecific({root}){
@@ -68,11 +69,16 @@ export class ControlCommand extends Control {
         this.commandTextElement = await this.$(".buttonlink");
         this.commandTextExtendedElement = await this.$(".buttonlinkextended");
         this.commandIconWrapperElement = await this.$(".commandiconwrapper");
+        this.keyboardShortcutElement = await this.$(".commandkeyboardshortcut");
 
         this.commandTextElement.innerHTML = this.command.getText();
         this.commandButton.setAttribute("aria-label",this.command.getText());
 
         UtilDOM.setInnerHTMLOrHide(this.commandIconWrapperElement,this.command.icon);
+        this.keyboardShortcutElement.onclick = async (e) => {
+            e.stopPropagation();
+            await EventBus.post(new KeyboardShortcutClicked(this.command));
+        }
 
         return root;
     }
@@ -93,5 +99,10 @@ export class ControlCommand extends Control {
     }
     set link(value){
         this.commandTextElement.href = value
+    }
+}
+class KeyboardShortcutClicked{
+    constructor(command){
+        this.command = command;
     }
 }
